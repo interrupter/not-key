8ddc8825d8ccc3594ec7f0ce7321ca5c5197fd5e
Aleksander Repin <alex.repin@mail.ru>
2020-08-19 14:43:47 +0300

    remove not-framework

--- src/controllers/root/ncKey.js
+++ src/controllers/root/ncKey.js
@@ -1,7 +1,5 @@
-/* global notFramework, document, confirm */
+/* global  document, confirm */
 const ERROR_DEFAULT = 'Что пошло не так.';
-
-import { notController, notCommon } from 'not-framework';
 
 import Common from '../common/index.js';
 
@@ -10,7 +8,9 @@ import {
 	Table as notTable,
 	Breadcrumbs,
 	UIError,
-	Form
+	Form,
+	notController,
+	notCommon
 } from 'not-bulma';
 
 const LABELS = {
@@ -391,91 +391,3 @@ class ncKey extends notController {
 }
 
 export default ncKey;
-
-
-/*
-this.setOptions('views', {
-	default:{
-		renderFromURL: false,
-		postfix: '',
-		name: 'content-list',
-		common: false
-	},
-	create: {
-		preload: {},
-		prefix: 'form-',
-		postfix: '',
-		action: 'create',
-		renderFromURL: false,
-		name:'content-edit',
-		targetQuery: '#form-place',
-		helpers: formHelpers
-	},
-	update: {
-		preload: {},
-		postfix: '',
-		prefix: 'form-',
-		action: 'update',
-		renderFromURL: false,
-		name:'content-edit',
-		targetQuery: '#form-place',
-		helpers: formHelpers
-	},
-	list: {
-		interface:	{
-			combined:	true
-		},
-		helpers:{
-			createURL: [this.getModelURL(),'create'].join('/')
-		},
-		targetQuery: '#table-place',
-		prefix: 'content-',
-		postfix: '',
-		endless: false,
-		renderFromURL: false,
-		common: false,
-		preload: {},
-		fields: [{
-			path: ':title',
-			title: 'Title',
-			searchable: true,
-			sortable: true
-		},{
-			path: ':key',
-			title: 'KEY',
-			searchable: true,
-			sortable: true
-		}, {
-			path: ':expiredAt',
-			title: 'Valid till',
-			sortable: true,
-			searchable: true
-		}, {
-			path: ':_id',
-			title: 'Действия',
-			preprocessor: (value) => {
-				return {
-					links:[
-						{
-							url: [this.getModelURL(), value, 'prolongForMonth'].join('/'),
-							title: 'Продлить на месяц'
-						},{
-							url: [this.getModelURL(), value, 'update'].join('/'),
-							title: 'Изменить'
-						},{
-							url: [this.getModelURL(), value, 'delete'].join('/'),
-							title: 'Удалить'
-						}
-					]
-				};
-			},
-			component: {
-				template: {
-					name: 'links'
-				}
-			}
-		}]
-	}
-});
-this.route(params);
-*/
