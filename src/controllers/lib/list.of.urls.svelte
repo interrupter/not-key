af50300b23c4eedafa7de2fa0b8bf41438e85b80
Aleksander Repin <alex.repin@mail.ru>
2021-03-01 18:53:37 +0300

    fix: UI listOfUrls refactored to not-nulma.FOrm compatible field interface

--- src/controllers/lib/list.of.urls.svelte
+++ src/controllers/lib/list.of.urls.svelte
@@ -1,19 +1,83 @@
 <script>
+  import {UICommon} from 'not-bulma';
+  import {
+    onMount,
+    createEventDispatcher
+  } from 'svelte';
+  let dispatch = createEventDispatcher();
 
-  import {onMount} from 'svelte';
+  export let inputStarted = false;
   export let value        = [];
+  export let placeholder   = 'List of urls';
+  export let fieldname     = 'list-of-urls';
+  export let rows = 10;
   export let textVersion  = '';
-  export let readonly     = false;
+  export let required = true;
+  export let readonly = false;
+  export let valid = true;
+  export let validated = false;
+  export let errors = false;
+  export let formErrors = false;
+  export let formLevelError = false;
 
   onMount(()=>{
     textVersion = value.join("\n");
   });
 
   $: value = textVersion.split("\n");
+  $: allErrors = [].concat(errors?errors:[], formErrors?formErrors:[]);
+  $: helper = allErrors?allErrors.join(', '): placeholder;
+  $: invalid = ((valid===false) || (formLevelError));
+  $: validationClasses = (valid===true || !inputStarted)?UICommon.CLASS_OK:UICommon.CLASS_ERR;
 
+  function onBlur(ev){
+    let data = {
+      field: fieldname,
+      value: ev.currentTarget.value
+    };
+    inputStarted = true;
+    dispatch('change', data);
+    return true;
+  }
+
+  function onInput(ev){
+    let data = {
+      field: fieldname,
+      value: ev.currentTarget.value
+    };
+    inputStarted = true;
+    dispatch('change', data);
+    return true;
+  }
 </script>
 
-
 <div class="control">
-  <textarea name="name" {readonly} disabled={readonly} rows="8" cols="80" bind:value={textVersion}></textarea>
+  <textarea
+    id="form-field-listOfUrls-{fieldname}"
+    name={fieldname}
+    bind:value={textVersion}
+    class="textarea {validationClasses}"
+    {readonly}
+    disabled={readonly}
+    invalid="{invalid}"
+    required={required}
+    on:change={onBlur} on:input={onInput}
+    {rows}
+    placeholder="{placeholder}"
+    aria-controls="input-field-helper-{fieldname}" aria-describedby="input-field-helper-{fieldname}"
+    ></textarea>
+    {#if validated === true }
+    <span class="icon is-small is-right">
+      {#if valid === true }
+      <i class="fas fa-check"></i>
+      {:else if (valid === false) }
+      <i class="fas fa-exclamation-triangle"></i>
+      {/if}
+    </span>
+    {/if}
 </div>
+<p class="help {validationClasses}" id="input-field-helper-{fieldname}">
+  {#if !(validated && valid) && (inputStarted) }
+  {helper}
+  {:else}&nbsp;{/if}
+</p>
