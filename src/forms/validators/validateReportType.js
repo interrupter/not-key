908f9a97101f5fb2815120b1e4dac55a147a5f89
interrupter <alex.repin@mail.ru>
2022-07-04 23:49:58 +0300

    fix: report and type validation in input

--- src/forms/validators/validateReportType.js
+++ src/forms/validators/validateReportType.js
@@ -1,17 +1,19 @@
-const notNode = require('not-node');
-const {notValidationError} = require('not-error');
-const {ERR_EMPTY_REPORT_OR_TYPE} = require('../../const');
+const notNode = require("not-node");
+const { notValidationError } = require("not-error");
+const { ERR_EMPTY_REPORT_OR_TYPE } = require("../../const");
 
-module.exports = async ({report, type}/*, validationEnvs*/) => {
-  if (!
-    (typeof report === 'undefined' || report === null ||
-      typeof type === 'undefined' || type === null)
-  ) {
-    throw new notValidationError(
-      `not-key:${ERR_EMPTY_REPORT_OR_TYPE}`,
-      {},
-      undefined,
-      {}
-    );
-  }
+module.exports = async ({ report, type } /*, validationEnvs*/) => {
+    if (
+        typeof report === "undefined" ||
+        report === null ||
+        typeof type === "undefined" ||
+        type === null
+    ) {
+        throw new notValidationError(
+            `not-key:${ERR_EMPTY_REPORT_OR_TYPE}`,
+            {},
+            undefined,
+            {}
+        );
+    }
 };
