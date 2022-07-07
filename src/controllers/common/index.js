a8ee62c072ef0e2f19473409d999b98be53a9a39
interrupter <alex.repin@mail.ru>
2022-07-07 14:56:02 +0300

    changed: UIs moved to common from root only

---
+++ src/controllers/common/index.js
@@ -0,0 +1,7 @@
+import UIListOfUrls from "./UIListOfURLs.svelte";
+import UIJSON from "./UIJSON.svelte";
+const uis = { UIListOfUrls, UIJSON };
+
+const manifest = {};
+
+export { manifest, uis };
--- src/controllers/root/index.js
+++ src/controllers/root/index.js
@@ -1,29 +1,21 @@
-import ncKey from './ncKey.js';
+import ncKey from "./ncKey.js";
 
 let manifest = {
-	router: {
-		manifest: [
-			ncKey.getRoutes()
-		]
-	},
-	menu:{
-		side: {
-			items: [{
-				id: 			'system.keys',
-				section: 'system',
-				title: 		'Ключи',
-				url: 			'/key'
-			}]
-		}
-	}
+    router: {
+        manifest: [ncKey.getRoutes()],
+    },
+    menu: {
+        side: {
+            items: [
+                {
+                    id: "system.keys",
+                    section: "system",
+                    title: "Ключи",
+                    url: "/key",
+                },
+            ],
+        },
+    },
 };
 
-import UIListOfUrls from '../lib/UIListOfURLs.svelte';
-import UIJSON from '../lib/UIJSON.svelte';
-const uis = {UIListOfUrls, UIJSON};
-
-export {
-	manifest,
-	uis,
-	ncKey
-};
+export { manifest, ncKey };
